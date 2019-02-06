eca3cd7dc7a1c28c2bd947a55e4242c3bf258692
Oli Nelson <olivernelson@icloud.com>
2019-02-06 16:53:03 -0500

    all working

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 let allBooks = []
 const listPanel = document.querySelector("#list-panel")
 const showPanel = document.querySelector("#show-panel")
-const myUser = {"id":1, "username":"pouros"}
+const myUser = {
+  "id": 1,
+  "username": "pouros"
+}
+const createButton = document.querySelector("#create-button")
 
-function apiGet(){
+function apiGet() {
   fetch("http://localhost:3000/books")
-  .then(function(r){
-    return r.json()
-  })
-  .then(function(r){
-    allBooks = r
-    showAllBooks()
+    .then(function(r) {
+      return r.json()
+    })
+    .then(function(r) {
+      allBooks = r
+      showAllBooks()
+    })
+}
+
+function apiCreate(name, image, description) {
+  fetch(`http://localhost:3000/books`,{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      title: name,
+      img_url: image,
+      description: description,
+      users: []
+    })
+  }).then(function(r){
+    return r.json
   })
 }
 
-function likeBookToggle(id, users){
-  fetch(`http://localhost:3000/books/${id}`,{
+function apiDelete(id) {
+  fetch(`http://localhost:3000/books/${id}`, {
+    method: "DELETE",
+  })
+}
+
+function likeBookToggle(id, users) {
+  fetch(`http://localhost:3000/books/${id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
@@ -24,22 +52,22 @@ function likeBookToggle(id, users){
     body: JSON.stringify({
       users: users
     })
-  }).then(function(r){
+  }).then(function(r) {
     return r.json()
   })
-}// end of likeBookToggle
+} // end of likeBookToggle
 
 function showAllBooks() {
-  for (let book of allBooks){
+  for (let book of allBooks) {
     listPanel.innerHTML +=
-    `<li data-id="${book.id}" class="book-list-item" id="book-list-${book.id}">${book.title}</li>`
+      `<li data-id="${book.id}" class="book-list-item" id="book-list-${book.id}">${book.title}</li>`
   }
 }
 
 function getBook(id) {
   let book = allBooks.find(function(element) {
-  return element.id == id
-})
+    return element.id == id
+  })
 
   showBook(book)
   return book
@@ -47,13 +75,13 @@ function getBook(id) {
 
 function showBook(book) {
   let usersHTML = ''
-  for (let user of book.users){
+  for (let user of book.users) {
     usersHTML += `<li data-id="${user.id}">${user.username}</li>`
   }
 
 
   showPanel.innerHTML =
-  `
+    `
   <h4>${book.title}</h4>
   <img src="${book.img_url}" alt="">
   <p>${book.description}</p>
@@ -61,10 +89,24 @@ function showBook(book) {
     ${usersHTML}
   </ul>
   <button id="read-book-button" data-bookId="${book.id}" data-userId="${myUser.id}"> Read Book </button>
+  <button id="delete-book-button" data-bookId="${book.id}"> Delete Book </button>
   `
 }
 
+function showCreateForm() {
+  showPanel.innerHTML =
+  `<form id="createForm">
+  	<input id="form-name" placeholder="name">
+  	<input id="form-image" placeholder="image">
+  	<input id="form-description" placeholder="description">
+    <button> Submit </button>
+  </form>`
+}
 
+function temporayIdGenerator() {
+  let ids = allBooks.map(book => book.id)
+  return ids.sort((a,b) => a + b)[0] + 1
+}
 
 
 
@@ -72,38 +114,70 @@ document.addEventListener("DOMContentLoaded", function() {
   apiGet()
 
 
-listPanel.addEventListener('click',function(e){
-  if (e.target.className === "book-list-item"){
-    id = e.target.dataset.id
-    getBook(id)
-  }
+  listPanel.addEventListener('click', function(e) {
+    if (e.target.className === "book-list-item") {
+      id = e.target.dataset.id
+      getBook(id)
+    }
+    if (e.target.id === "create-button") {
+      showCreateForm()
+    }
 
 
-})//end of listPanel listener
+  }) //end of listPanel listener
 
-showPanel.addEventListener('click',function(e){
-  if(e.target.id === 'read-book-button'){
-    console.log("you pressed the read book button");
-    id = e.target.dataset.bookid
-    users = getBook(id).users
-    book = getBook(id)
+  showPanel.addEventListener('click', function(e) {
+    if (e.target.id === 'read-book-button') {
+      // console.log("you pressed the read book button");
+      id = e.target.dataset.bookid
+      users = getBook(id).users
+      book = getBook(id)
 
-    if(users.some(user => user.username === myUser.username)){
+      if (users.some(user => user.username === myUser.username)) {
 
-    let filteredUsers = users.filter(user => user.username != myUser.username);
-    likeBookToggle(id, filteredUsers)
-    showBook(book)
-} else{
+        let filteredUsers = users.filter(user => user.username != myUser.username);
+        likeBookToggle(id, filteredUsers)
+        book.users = filteredUsers
+        showBook(book)
 
-
-    users.push(myUser)
-    likeBookToggle(id, users)
-    showBook(book)
-}
+      } else {
 
 
-  }
-})
+        users.push(myUser)
+        likeBookToggle(id, users)
+        showBook(book)
+      }
+
+
+    }
+    if (e.target.id === 'delete-book-button') {
+        id = e.target.dataset.bookid
+
+      apiDelete(id)
+      showPanel.innerHTML = ``
+      document.querySelector(`#book-list-${id}`).remove()
+    }
+  }) //end of showpanel listener
+
+  showPanel.addEventListener('submit',function(e){
+    let name = document.querySelector('#form-name').value
+    let image = document.querySelector('#form-image').value
+    let description = document.querySelector('#form-description').value
+    let id = temporayIdGenerator()
+    e.preventDefault()
+    apiCreate(name, image, description)
+    showPanel.innerHTML =
+    `
+  <h4>${name}</h4>
+  <img src="${image}" alt="">
+  <p>${description}</p>
+  <ul id="users">
+
+  </ul>
+  <button id="read-book-button" data-bookId="${id}" data-userId="${id}"> Read Book </button>
+  <button id="delete-book-button" data-bookId="${id}"> Delete Book </button>
+  `
+  })
 
 
 });
